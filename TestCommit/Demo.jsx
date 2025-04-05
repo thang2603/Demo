907758f149611a75ec4c0af51740fd243d79ad25
thang2603 <thangc2k53@gmail.com>
2025-04-05 21:26:25 +0700

    My final cleaned-up commit

---
+++ TestCommit/Demo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Demo = () => {
+  return <div>Demo 4</div>;
+};
+
+export default Demo;
---
+++ TestCommit/TestCommit.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestCommit = () => {
+  return <div>Test Commit 4</div>;
+};
+
+export default TestCommit;
